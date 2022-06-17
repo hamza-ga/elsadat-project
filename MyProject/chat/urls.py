0ab155a3ef82f82c2ca28644153132e6256f7c7e
hamza-ga <hhamzagamal@gmail.com>
2022-06-17 17:49:07 +0200

    urls chats update

--- MyProject/chat/urls.py
+++ MyProject/chat/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
-from members import views  
+from members.views import SignupPatientView
 
 
 urlpatterns = [
-    path('', views.SignupPatientView.as_view(), name='signup'),
+    path('', SignupPatientView.as_view(), name='signup'),
     path('chat/', views.chat_view, name='chats'),
     path('chat/<int:sender>/<int:receiver>/', views.message_view, name='chat'),
     path('api/messages/<int:sender>/<int:receiver>/', views.message_list, name='message-detail'),
